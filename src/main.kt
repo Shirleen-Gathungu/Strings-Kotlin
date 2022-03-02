9ed66f5e0296ace71f3b2dd202301c65ee7286d2
Shirleen19 <muthonishirleen2002@gmail.com>
2022-03-02 11:40:43 +0300

    Strings

---
+++ src/main.kt
@@ -0,0 +1,111 @@
+//fun main() {
+
+
+//    var name = "Juliet"
+//    var action = "did not do her assignment"
+//    var qualifier = "in time"
+//    var statement = "$name $action $qualifier"
+//    println(statement)
+//    helloName("Shirleen")
+
+//    var fruit="Orange"
+//println(fruit[0])
+//    println(fruit[1])
+//    println(fruit[2])
+//    println(fruit[3])
+//    println(fruit[4])
+//    println(fruit[5])
+
+//    var fruit = "Banana"
+//    var f=(fruit[2].toString()+fruit[3]+fruit[4])
+//    println(f)
+//
+//    var x="abcd"
+//    var v="abcd"
+//   println(x.equals(v))
+//
+//    var text="shirleeen"
+//    println(text.uppercase())
+//    println(text.capitalize())
+//
+//
+//    var o =""
+//    var s =" "
+//
+//    println(o.isEmpty())
+//    println(s.isBlank())
+//
+//    var word= " Susan Limbo"
+//    println(word)
+//    println(word.trim())
+//    println(word.trimStart())
+//    println(word.trimEnd())
+//
+//    var sentence = "Atlanta is a big city"
+//    println(sentence.replace("Atlanta","Newyork"))
+//
+//    var word ="Jack is a good person"
+//    println(word.replace("good","bad"))
+//
+//    var message = word.split(" ")
+//    println(message)
+//
+//    var details = "Janet Museveni,Ugandan,25672345678.40328970,Kampala"
+//    var d= details.split(" ")
+////    println(d[2])
+//    var display = myPin(20,2002)
+//    println(display)
+//}
+//
+//    fun myPin(age:Int,year:Int):String {
+//        var pin = year.toString() + age
+//        return pin
+//    }
+
+      
+
+
+//}
+//fun helloName(name:String){
+//    var message=("Hello"+" "+name)
+//  println(message)
+//
+//}
+
+
+
+    fun main(){
+        printString()
+        var message=myAge()
+        println(message)
+
+    }
+
+
+
+
+fun printString(){
+        var name = "AkiraChix"
+        var display=(name[2].toString()+name[3]+name[4])
+        println(display)
+    }
+
+    fun myAge(){
+        fun myAge(word:String,age:Int):String{
+            var x=word
+            var age=20
+            var statement="Hi,my name is $word,I am $age year old"
+            return statement
+        }
+
+        fun stringLength(name:String){
+            var name = "Shirleen"
+            println(name.length)
+
+        }
+        fun myName(){
+            var name="Shirleen"
+            var name2="Dylan"
+
+
+        }
